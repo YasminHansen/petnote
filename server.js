d96f192927e5fb843993550eba9e43bd2348cd61
Yasmin Hansen <yaa-h@hotmail.com>
2020-05-24 18:12:06 -0300

    alterando server.js

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const app = express();
  
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
  
-// app.get('/*', (req, res) => {
-// res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-// });
-
 app.get('*', (req, res) => {
-    res.sendFile(`./dist/${nomeApp}/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
  
 app.listen(process.env.PORT || 8080);
